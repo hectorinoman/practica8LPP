330cd67ac6e47a859b9707a648cb28b37cae60c9
hectorinoman <hectorinoman@gmail.com>
2015-11-18 17:41:05 +0000

    error al cerrar la clase padre

--- lib/referencia/clase.rb
+++ lib/referencia/clase.rb
@@ -11,9 +11,38 @@ class Bibliografia
         @fecha = fecha
         @isbn = isbn
     end
+end    
+
     
-    def to_s
-        "(#{@autor},#{@titulo},#{@serie},#{@editorial},#{@edicion},#{@fecha},#{@isbn})"
+    
+    class Book < Bibliografia
+    
+    attr_accessor :book
+    
+    def initialize(autor, titulo, serie, editorial, edicion, fecha, book)
+        super(autor, titulo, serie, editorial, edicion, fecha)
+        @book = book
+    end
     end
 
-end
+    class Magazine < Bibliografia
+        
+        attr_accessor :magazine
+        
+        def initialize(autor, titulo, serie, editorial, edicion, fecha, magazine)
+            super(autor, titulo, serie, editorial, edicion, fecha)
+            @magazine = magazine
+        end
+    end
+
+    class Electronic < Bibliografia
+        
+       attr_accessor :electronic
+        
+        def initialize(autor, titulo, serie, editorial, edicion, fecha, electronic)
+            super(autor, titulo, serie, editorial, edicion, fecha)
+            @electronic = electronic
+        end
+    
+    end
+
--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -7,7 +7,7 @@ describe Referencia do
         @referencia2 = Bibliografia.new(["Scott Chacon"], "Pro Git 2009th Edition", "Pro", "Apress", "2009 edition", "August 27, 2009", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
         @referencia3 = Bibliografia.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language", "Serie", "O’Reilly Media", " 1 edition", "February 4, 2008", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
         @referencia4 = Bibliografia.new(["David Chelimsky", "Dave Astels", " Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "The Facets of Ruby", "Pragmatic Bookshelf", "1 edition", "December 25, 2010", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371",])
-        @referencia5 = Bibliografia.new(["Richard E. Silverman"], "Git Pocket Guide", "Serie", "O’Reilly Media", "1 edition",  "August 2, 2013", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])    
+        @referencia5 = Magazine.new(["Richard E. Silverman"], "Git Pocket Guide", "Serie", "O’Reilly Media", "1 edition",  "August 2, 2013", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])    
         
         @nodo1  = Nodo.new(@referencia1)
         @nodo2 = Nodo.new(@referencia2)
